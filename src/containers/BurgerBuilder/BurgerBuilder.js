1f390e7e729f5da758da4b12c2e6ea51169d7b54
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-09-26 13:32:11 +0200

    Done Building HardCoded Burger 🍔🍔

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 
 import Auxx from "../../hoc/Auxx";
 
+import Burger from "../../components/Burger/Burger";
+
 export class BurgerBuilder extends Component {
   render() {
     return (
       <Auxx>
-        <div> Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Auxx>
     );
