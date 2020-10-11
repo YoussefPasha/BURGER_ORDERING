411a639b9e89002c3fd2a836a483fe5a9f862bb6
YoussefSedekPasha <45726395+YoussefPasha@users.noreply.github.com>
2020-10-11 15:55:11 +0200

    Add sidebar & styling responsive wepapp

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import Auxx from "../../hoc/Auxx";
 import "./Layout.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const Layout = (props) => (
   <Auxx>
     <div>
       <Toolbar />
+      <SideDrawer />
     </div>
     <main className="Content">{props.children}</main>
   </Auxx>
